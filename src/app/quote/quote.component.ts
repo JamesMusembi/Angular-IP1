952a83a742814743ac85f9144ffc6c83f9536510
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-10 20:01:58 +0300

    quote.component.ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes : Quote []=[
-    new Quote(1,'isaac kiptoo','James Musembi', 'I get a lot of letters from people. They say, \‘I want to be a writer. What should I do?\’ I tell them to stop writing to me and get on with it.'),
-    new Quote(2,'isaac kiptoo','James Musembi', 'I get a lot of letters from people. They say, \‘I want to be a writer. What should I do?\’ I tell them to stop writing to me and get on with it.'),
-    new Quote(3,'isaac kiptoo','James Musembi', 'I get a lot of letters from people. They say, \‘I want to be a writer. What should I do?\’ I tell them to stop writing to me and get on with it.'),
-    new Quote(4,'isaac kiptoo','James Musembi', 'I get a lot of letters from people. They say, \‘I want to be a writer. What should I do?\’ I tell them to stop writing to me and get on with it.')
+    new Quote(1,'Thomas Edison','James Musembi', 'Many of life failures are people who did not realize how close they were to success when they gave up'),
+    new Quote(2,'Hellen Keller','James Musembi', 'Life is a succession of lessons which must be lived to be understood'),
+    new Quote(3,'Kwame Albert','James Musembi', 'The healthiest response to life is joy.'),
+    new Quote(4,'Dickson ','James Musembi', 'Life is like a coin. You can spend it any way you wish, but you only spend it once.')
   ]
 
   constructor() { }
