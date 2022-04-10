9d1ee92fca8ef6ca15311b57a237e52137c00e53
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-10 20:08:51 +0300

    modified quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,7 +11,7 @@ export class QuoteComponent implements OnInit {
     new Quote(1,'Thomas Edison','James Musembi', 'Many of life failures are people who did not realize how close they were to success when they gave up'),
     new Quote(2,'Hellen Keller','James Musembi', 'Life is a succession of lessons which must be lived to be understood'),
     new Quote(3,'Kwame Albert','James Musembi', 'The healthiest response to life is joy.'),
-    new Quote(4,'Dickson ','James Musembi', 'Life is like a coin. You can spend it any way you wish, but you only spend it once.')
+    new Quote(4,'Dickson Kibe','James Musembi', 'Life is like a coin. You can spend it any way you wish, but you only spend it once.')
   ]
 
   constructor() { }
