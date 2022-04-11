1f35f1ebd5c16a2ca56ec3b906da741be7cbd739
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-11 17:11:26 +0300

    form validation

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  @Output() quoteSubmitEvent=new EventEmitter<Quote>();
+  quoteSubmit(form:NgForm){
+   let formData=form.value;
+   let newDate= new Date(formData.date);
+   formData.date=newDate;
+   formData.downVote=0;
+   formData.upVote=0;
+   this.quoteSubmitEvent.emit(formData)
+   console.log(formData)
+  }
 
   constructor() { }
 
