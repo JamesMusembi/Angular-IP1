076156c1b09c1dd491b9cb5fbb13d8b99a825dde
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-11 09:21:17 +0300

    modified class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,3 @@
 export class Quote {
-    constructor( public id:number, public author:string, public submited:string, public Quote:string){}
+    constructor( public id:number, public author:string, public submited:string, public Quote:string , public date:Date,  public downVote:number,public upVote:number){}
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes : Quote []=[
-    new Quote(1,'Thomas Edison','James Musembi', 'Many of life failures are people who did not realize how close they were to success when they gave up'),
-    new Quote(2,'Hellen Keller','James Musembi', 'Life is a succession of lessons which must be lived to be understood'),
-    new Quote(3,'Kwame Albert','James Musembi', 'The healthiest response to life is joy.'),
-    new Quote(4,'Dickson Kibe','James Musembi', 'Life is like a coin. You can spend it any way you wish, but you only spend it once.')
+    new Quote(1,'Thomas Edison','James Musembi', 'Many of life failures are people who did not realize how close they were to success when they gave up',new Date('2015,05,05'),0,0),
+    new Quote(2,'Hellen Keller','James Musembi', 'Life is a succession of lessons which must be lived to be understood',new Date('2015,05,05'),0,0),
+    new Quote(3,'Kwame Albert','James Musembi', 'The healthiest response to life is joy', new Date('2015,05,05'),0,0),
+    new Quote(4,'Dickson Kibe','James Musembi', 'Life is like a coin. You can spend it any way you wish, but you only spend it once', new Date('2015,05,05'),0,0)
   ]
 
   constructor() { }
