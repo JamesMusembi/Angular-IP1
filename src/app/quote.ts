63aec6f4aeff7a92f6e16fd67069eb533454b464
JamesMusembi <james.musembi@student.moringaschool.com>
2022-04-11 19:34:25 +0300

    Auto-generated commit [ci skip]

--- src/app/quote.ts
+++
@@ -1,3 +0,0 @@
-export class Quote {
-    constructor( public title:string, public author:string, public submited:string, public Quote:string , public date:Date,  public downVote:number,public upVote:number){}
-}
